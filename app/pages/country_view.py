46fee8c15f62bd3c2abb1a604ca6fc0d998b038a
HARIHARAN B - Analytics <hhariharan203@gmail.com>
2026-03-31 21:54:55 +0530

    Update country_view.py

--- app/pages/country_view.py
+++ app/pages/country_view.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import numpy as np
 import pandas as pd
-import plotly.express as px
 import streamlit as st
-import shap
 
-from app.utils import (
+from utils import (
     load_data,
     load_model,
     add_probabilities,
