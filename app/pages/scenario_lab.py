88128d3c8c1eb05fce56ab1bcb266bc22ed886f2
HARIHARAN B - Analytics <hhariharan203@gmail.com>
2026-04-02 01:06:59 +0530

    scenario_lab.py

--- app/pages/scenario_lab.py
+++ app/pages/scenario_lab.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import utils
 
+df = utils.load_data()
+c = st.session_state.get("country","USA")
+
+row = utils.latest(df,c).copy()
+
+infl = st.slider("Inflation Shock", -5,10,2)
+gdp  = st.slider("GDP Shock", -10,5,-2)
+
+row["inflation"] += infl
+row["gdp_growth"] += gdp
+
+score = utils.predict(row)[0]
+
+st.metric("Scenario Risk", round(score,3))
