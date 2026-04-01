35199f9b31aca6e90c56556df683e2e7112de651
HARIHARAN B - Analytics <hhariharan203@gmail.com>
2026-04-02 01:04:19 +0530

    global_risk.py

--- app/pages/global_risk.py
+++ app/pages/global_risk.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import utils
+import pydeck as pdk
 
+df = utils.load_data()
+risk = utils.global_risk(df)
+
+risk["lat"] = 20
+risk["lon"] = 0
+
+layer = pdk.Layer(
+    "ColumnLayer",
+    data=risk,
+    get_position='[lon, lat]',
+    get_elevation='risk_score*1000000',
+)
+
+st.pydeck_chart(pdk.Deck(layers=[layer]))
