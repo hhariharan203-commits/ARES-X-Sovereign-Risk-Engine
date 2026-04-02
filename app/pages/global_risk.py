65319ac1714059a94b42aa9089f21241e8c2bd75
HARIHARAN B - Analytics <hhariharan203@gmail.com>
2026-04-02 09:59:10 +0530

    Update global_risk.py

--- app/pages/global_risk.py
+++ app/pages/global_risk.py
@@ -1,5 +1,15 @@
 import streamlit as st
 import utils
+import plotly.express as px
 
-df = utils.load_data()
-st.dataframe(utils.global_risk(df))
+df = utils.add_risk(utils.load_data())
+
+latest = df.sort_values("year").groupby("country").tail(1)
+
+st.title("🌍 Global Risk Landscape")
+
+top = latest.sort_values("risk_score", ascending=False).head(10)
+
+fig = px.bar(top, x="country", y="risk_score", color="risk_score")
+
+st.plotly_chart(fig, use_container_width=True)
