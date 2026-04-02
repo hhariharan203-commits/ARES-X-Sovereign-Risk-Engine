19499bc391c851293dfde304ecd7d4e7b0f0827c
HARIHARAN B - Analytics <hhariharan203@gmail.com>
2026-04-02 08:53:30 +0530

    forecast.py

--- app/pages/forecast.py
+++ app/pages/forecast.py
@@ -1,16 +1,11 @@
 import streamlit as st
 import utils
-import plotly.graph_objects as go
 
 df = utils.load_data()
 c = st.session_state.get("country","USA")
 
 row = utils.latest(df,c)
+
 sims = utils.monte_carlo(row)
 
-fig = go.Figure()
-fig.add_histogram(x=sims)
-
-st.plotly_chart(fig)
-
-st.metric("Expected Risk", round(sims.mean(),3))
+st.metric("Expected Risk", sims.mean())
