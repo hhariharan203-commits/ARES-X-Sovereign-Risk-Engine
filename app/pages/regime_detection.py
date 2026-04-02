01bbfef855b9763825907235db4cd2f0346685f3
HARIHARAN B - Analytics <hhariharan203@gmail.com>
2026-04-02 08:55:51 +0530

    regime_detection.py

--- app/pages/regime_detection.py
+++ app/pages/regime_detection.py
@@ -4,6 +4,4 @@ import utils
 df = utils.load_data()
 c = st.session_state.get("country","USA")
 
-d = utils.detect_regime(df,c)
-
-st.write("Current Regime:", d.iloc[-1]["regime"])
+st.dataframe(utils.detect_regime(df,c))
