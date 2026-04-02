581c6028d8aa79404a05b436cda43fdfaba30148
HARIHARAN B - Analytics <hhariharan203@gmail.com>
2026-04-02 08:55:17 +0530

    model_performance.py

--- app/pages/model_performance.py
+++ app/pages/model_performance.py
@@ -1,15 +1,4 @@
 import streamlit as st
 import utils
-import plotly.graph_objects as go
 
-m = utils.metrics()
-
-st.metric("ROC AUC", m["roc_auc"])
-
-fig = go.Figure(go.Indicator(
-    mode="gauge+number",
-    value=m["roc_auc"],
-    gauge={'axis':{'range':[0,1]}}
-))
-
-st.plotly_chart(fig)
+st.json(utils.metrics())
