a85507bcb5a719fda930581a1dfef12bb73de262
HARIHARAN B - Analytics <hhariharan203@gmail.com>
2026-04-02 10:02:18 +0530

    Update regime_detection.py

--- app/pages/regime_detection.py
+++ app/pages/regime_detection.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import utils
+import numpy as np
 
-df = utils.load_data()
-c = st.session_state.get("country","USA")
+df = utils.add_risk(utils.load_data())
 
-st.dataframe(utils.detect_regime(df,c))
+df["regime"] = np.where(df["risk_score"]>0.8,"Crisis",
+                np.where(df["risk_score"]>0.5,"Stress","Stable"))
+
+st.title("Regime Distribution")
+
+st.bar_chart(df["regime"].value_counts())
