930da2f610a56703d7f5acd2069e1b328747e057
HARIHARAN B - Analytics <hhariharan203@gmail.com>
2026-04-02 08:57:02 +0530

    portfolio_impact.py

--- app/pages/portfolio_impact.py
+++ app/pages/portfolio_impact.py
@@ -9,8 +9,8 @@ row = utils.latest(df,c)
 
 alloc = st.slider("Exposure %",0,100,50)
 
-score = utils.predict(row)[0]
+score,_ = utils.predict_full(row)
 
-loss = alloc * score * np.random.uniform(0.8,1.2)
+loss = alloc * score
 
 st.metric("Expected Loss %", round(loss,2))
